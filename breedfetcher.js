39f1e5a1e315526bd6f9b158da4bc2ea48d9237e
easydoesit <michael.grills@gmail.com>
2022-11-20 06:32:51 +0000

    refactored, add index.js

--- breedfetcher.js
+++ breedfetcher.js
@@ -1,26 +1,28 @@
 const request = require("request");
 
-// command line arguments
-const args = process.argv.slice(2);
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`;
+const fetchBreedDescription = function(breedName, callback) {
 
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
+  // request info from the server
 
-// request info from the server
-
-request(url, function(error, response, body) {
-  if (error) {
-    if (error.code === 'ENOTFOUND') {
-      console.error('error:', error);
-      return;
+  request(url, function(err, response, body) {
+    if (err !== null) {
+      if (err.code === 'ENOTFOUND') {
+        callback(err,null);
+        return;
+      }
+    } else {
+      const data = JSON.parse(body);
+  
+      if (data.length === 0) {
+        callback("Sorry, that breed was not found", null);
+        return;
+      }
+      desc = data[0].description;
+      callback(null, desc);
     }
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Sorry, that breed was not found");
-    return;
-  }
-  //console.log('statusCode:', response && response.statusCode);
-  console.log(data[0].description);
-  //console.log(typeof data);
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
